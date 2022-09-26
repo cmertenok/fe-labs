9fb8c1435476b1a22112dfd7ad9b825d11eac7b2
Artem <artempozitiv56@gmail.com>
2022-09-26 11:36:48 +0200

    lab4 task 1

--- lab4/script.js
+++ lab4/script.js
@@ -1,1 +1,24 @@
 'use strict';
+/* Task 1 */
+const elemetId = document.getElementById('task1');
+const elemetClass = document.querySelector('.task1');
+
+function generateRandomNum() {
+    return Math.floor(Math.random() * 255);
+}
+
+function generateRandomColor() {
+    return `rgb(${generateRandomNum()},${generateRandomNum()},${generateRandomNum()})`;
+}
+ 
+function paintFirst() {
+    elemetId.style.background = generateRandomColor();
+    elemetId.style.color = generateRandomColor();
+}
+
+function paintSecond() {
+    elemetClass.style.background = generateRandomColor();
+    elemetClass.style.color = generateRandomColor();
+}
+
+/* Task 2 */
